1bde72d0a5d9f979f1ffadacaed3b3f187855f12
EddieSadat <68033235+EddieSadat@users.noreply.github.com>
2020-07-20 13:34:16 -0700

    collision update
    Ball and Paddle collision added

--- main2.py
+++ main2.py
@@ -1,4 +1,5 @@
 import pygame
+from math import sqrt
 
 class Ball():
     def __init__(self, x_pos, y_pos, x_vel, y_vel, radius, win):     #I can remove if too long
@@ -22,26 +23,92 @@ class Ball():
 
         #friction
         self.timer += 1
-        if self.timer == 60:
-            if self.y_vel > 0:
+        if self.timer == 60: #the problem is here
+            if self.y_vel > 1:
                 self.y_vel -= 1
-                if self.y_vel < 0:
-                    self.y_vel = 0
-            elif self.y_vel < 0:
+                if self.y_vel < 1:
+                    self.y_vel = 1
+            elif self.y_vel < 1:
                 self.y_vel += 1
-                if self.y_vel > 0:
-                    self.y_vel = 0
-            self.timer = 0
+                if self.y_vel > 1:
+                    self.y_vel = 1
+            self.timer = 1
 
         #vel of ball
         self.x_pos += self.x_vel
         self.y_pos += self.y_vel
     
 
+class Paddle():
+    def __init__(self, x_pos, y_pos, win):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.x_vel = 0
+        self.y_vel = 0
+        self.win = win
+        self.width = 10
+        self.height = 50
+
+    def draw(self):
+        pygame.draw.rect(self.win, (255, 255, 255), (self.x_pos, self.y_pos, self.width, self.height))
+    
+    def up(self):
+        if self.y_pos >= 10:
+            self.y_vel -= 2
+            if self.y_vel < -10:
+                self.y_vel = -10
+
+    def  down(self):
+        if self.y_pos <= 440:
+            self.y_vel += 2
+            if self.y_vel > 10:
+                self.y_vel = 10
+
+    def update(self):
+        self.y_pos += self.y_vel
+        if self.y_pos < 0:
+            self.y_pos = 0
+            self.y_vel = 0
+        if self.y_pos > 450:
+            self.y_pos = 450
+            self.y_vel = 0
+
+    def collision(self, ball: Ball):
+        cx = ball.x_pos
+        cy = ball.y_pos
+        radius = ball.radius
+        rx = self.x_pos
+        ry = self.y_pos
+        rw = self.width
+        rh = self.height
+
+        testX = cx
+        testY = cy
+
+        if (cx < rx):
+            testX = rx  
+        elif (cx > rx+rw):
+            testX = rx+rw
+        if (cy < ry):
+            testY = ry      
+        elif (cy > ry+rh): 
+            testY = ry+rh
+
+        distX = cx-testX
+        distY = cy-testY
+        distance = sqrt( (distX*distX) + (distY*distY) )
+
+        
+        if (distance <= radius):
+            return True
+        
+        return False
+
 
 class Game():
     def __init__(self, width, height):   #We dont need to  keep balls or score, both start at 0
         self.balls = []
+        self.paddles = []
         self.score = 0
         self.width = width
         self.height = height
@@ -51,16 +118,64 @@ class Game():
 
     def run(self):
         run = True
-        ball = Ball(250, 250, 5, 3, 5, self.win)
+        
+        self.balls.append(Ball(250, 250, 5, 3, 5, self.win))
+        self.balls.append(Ball(300, 250, 5, 3, 5, self.win))
+        paddle1 = Paddle(20, 200, self.win)
+        paddle2 = Paddle(470, 200, self.win)
+
+        self.paddles.append(paddle1)
+        self.paddles.append(paddle2)
+        
+
         while run:
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_UP]:
+                paddle1.up()
+
+            if keys[pygame.K_DOWN]:
+                paddle1.down()
+
+
+            if keys[pygame.K_w]:
+                paddle2.up()
+
+            if keys[pygame.K_s]:
+                paddle2.down()
+            
+
             pygame.time.delay(50)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
             self.win.fill((0, 0, 0))
-            ball.draw()
-            ball.move()
+
+            for ball in self.balls:
+                ball.move()
+
+                
+            for paddle in self.paddles:
+                paddle.update()
+                
+            for paddle in self.paddles:
+                for  ball in self.balls:
+                    if paddle.collision(ball):
+                        ball.x_vel *= -1
+
+            for ball in self.balls:
+                ball.draw()
+
+                
+            for paddle in self.paddles:
+                paddle.draw()
+                    
+                    
+
+
             pygame.display.update()
+
+
 
 Game(500, 500)
 
